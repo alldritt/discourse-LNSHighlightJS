05b69738846ce0b98193265db043237635bd4996
Mark Alldritt <alldritt@latenightsw.com>
2016-08-18 15:21:16 -0700

    Initial check in

---
+++ plugin.rb
@@ -0,0 +1,49 @@
+# name: 	LNSHighlightJS
+# about: 	Patch HighlightJs module to provide applescript:// links
+# version: 	0.1
+# authors: 	Mark Alldritt
+# url:		https://github.com/alldritt/discourse-LNSHighlightJS
+
+
+after_initialize do
+
+  module HighlightJs
+	  def self.languages
+		Dir.glob(File.dirname(__FILE__) << "/../../lib/assets/lang/*.js").map do |path|
+		  File.basename(path)[0..-4]
+		end
+	  end
+
+	  def self.bundle(langs)
+		path = File.dirname(__FILE__) << "/../../lib/assets/"
+
+		result = File.read(path + "highlight.js")
+		
+		# Patch the contents of highlight.js (now in result) to incldue our code...
+		newCode = "if (name == \"applescript\") {
+  result = \"<p><strong><a class=\\\"hljs-title\\\" href=\\\"sdapplescript://com.apple.scriptdebugger?action=new&script=\" + encodeURIComponent(value) + \"\\\">Open in Script Debugger</a></strong></p>\" + result;
+}"
+		result = result.sub("return{", newCode + "return{")
+    	
+		langs.each do |lang|
+		  begin
+			result << "\n" << File.read(path + "lang/#{lang}.js")
+		  rescue Errno::ENOENT
+			# no file, don't care
+		  end
+		end
+
+		result
+	  end
+
+	  def self.version(lang_string)
+		(@lang_string_cache ||= {})[lang_string] ||=
+		  Digest::SHA1.hexdigest(bundle lang_string.split("|"))
+	  end
+
+	  def self.path
+		"/highlight-js/#{Discourse.current_hostname}/#{version SiteSetting.highlighted_languages}.js"
+	  end
+  end
+
+end
