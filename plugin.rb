5a52a24cae62b0857467ebfa6a93b1e57a61a0a8
Mark Alldritt <alldritt@latenightsw.com>
2016-08-18 18:21:51 -0700

    more debugging

--- plugin.rb
+++ plugin.rb
@@ -52,3 +52,45 @@ after_initialize do
   end
 
 end
+
+  module HighlightJs
+	  def self.languages
+		Dir.glob(File.dirname(__FILE__) << "/../../lib/assets/lang/*.js").map do |path|
+		  File.basename(path)[0..-4]
+		end
+	  end
+
+	  def self.bundle(langs)
+	    puts("LNSHighlightJS.b")
+
+		path = File.dirname(__FILE__) << "/../../lib/assets/"
+
+		result = File.read(path + "highlight.js")
+		
+		# Patch the contents of highlight.js (now in result) to incldue our code...
+		matchCode = "return{"
+		newCode = "if (name == \"applescript\") {
+  result = \"<p><strong><a class=\\\"hljs-title\\\" href=\\\"sdapplescript://com.apple.scriptdebugger?action=new&script=\" + encodeURIComponent(value) + \"\\\">Open in Script Debugger</a></strong></p>\" + result;
+}"
+		result = "/*mark*/" + result.sub(matchCode, newCode + matchCode)
+    	
+		langs.each do |lang|
+		  begin
+			result << "\n" << File.read(path + "lang/#{lang}.js")
+		  rescue Errno::ENOENT
+			# no file, don't care
+		  end
+		end
+
+		result
+	  end
+
+	  def self.version(lang_string)
+		(@lang_string_cache ||= {})[lang_string] ||=
+		  Digest::SHA1.hexdigest(bundle lang_string.split("|"))
+	  end
+
+	  def self.path
+		"/highlight-js/#{Discourse.current_hostname}/#{version SiteSetting.highlighted_languages}.js"
+	  end
+  end
